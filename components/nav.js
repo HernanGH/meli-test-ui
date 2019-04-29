036c8f2e889e4c0e06a624ea3903d1db64204054
Hernan Condori <hjcondori04@gmail.com>
2019-04-29 00:03:04 -0300

    Head - Dinamic metada was added to Head component. - Fix in Nav component search word is missing.

--- components/nav.js
+++ components/nav.js
@@ -11,27 +11,28 @@ class Nav extends React.Component {
 
   onChange = (e) => {
     const { value } = e.target;
-    this.setState({searchWord: value});
+    this.setState({searchWord: value, searchedWord: ''});
   };
 
   render() {
     const { searchWord } = this.state;
+    const { searchedWord } = this.props;
     const searchUrl = searchWord ? `/items?search=${searchWord}` : `/`;
     return (
       <Row type='flex' justify='center' className='nav-container' >
         <nav className='nav-search' >
           <ul>
             <li>
-              <Link prefetch href='/'>
-                <img src='./static/assets/Logo_ML.png' />
+              <Link href='/'>
+                <img src={`http://localhost:3000/static/assets/Logo_ML.png`} />
               </Link>
             </li>
             <li className='search-container'>
               <div className='search-box'>
-                <Input value={searchWord} placeholder='Nunca dejes de buscar' onChange={this.onChange} onPressEnter={() => Router.push(searchUrl)} />
-                <Link href={searchUrl}>
+                <Input value={searchWord || searchedWord} placeholder='Nunca dejes de buscar' onChange={this.onChange} onPressEnter={() => Router.push(searchUrl)} />
+                <Link prefetch href={searchUrl} >
                   <Button>
-                    <img  src='../static/assets/ic_Search.png' />
+                    <img  src='http://localhost:3000/static/assets/ic_Search.png' />
                   </Button>
                 </Link>
               </div>
